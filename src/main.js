f4cc9ded616986a5c7704544fbcc03ecf9b7c578
liuchaofan <liuchaofan@baidu.com>
2017-10-11 10:48:45 +0800

    修改了在没有安装router的情况下热更新的代码

--- src/main.js
+++ src/main.js
@@ -24,10 +24,10 @@ router.start();
 
 // hmr 更新逻辑
 if (process.env.NODE_ENV === 'development' && module.hot) {
-    module.hot.data = {app, router};
+    module.hot.data = {app{{#router}}, router{{/router}}};
 
     // 接受热更新的依赖数组
-    module.hot.accept(['./routes', './App.san'], () => {
+    module.hot.accept([{{#router}}'./routes', {{/router}}'./App.san'], () => {
 
         // 销毁旧的app
         module.hot.data.app.dispose();
@@ -35,19 +35,25 @@ if (process.env.NODE_ENV === 'development' && module.hot) {
         module.hot.data.router.stop();
         // 这里要注意，router中存活的组件实例和App是没有父子关系的
         // 所以要手动dispose
+        {{#router}}
         module.hot.data.router.routeAlives.forEach(item => {
             item.component.dispose();
         });
         module.hot.data.router = null;
+        {{/router}}
 
         // 创建新的app和router
         const app = new App();
+        {{#router}}
         const router = new Router();
+        {{/router}}
         app.attach(document.getElementById('app'));
+        {{#router}}
         routes.forEach(route => router.add(route));
         router.start();
+        {{/router}}
 
         // app传递给module.hot.data以便下次更新时销毁
-        module.hot.data = {app, router};
+        module.hot.data = {app{{#router}}, router{{/router}}};
     });
 }
